b33f42b314b5f3a2735f6f73a221273d204bf655
theedigerati <39467790+theedigerati@users.noreply.github.com>
2024-02-27 06:28:53 +0100

    add implementation for LinearSearch & BinarySearch

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,19 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+
+    while (lo < hi) {
+        const mid = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[mid];
+
+        if (needle === v) {
+            return true;
+        }
+        if (needle > v) {
+            lo = lo + 1;
+        } else {
+            hi = mid;
+        }
+    }
+    return false;
+}
