23c6ae7af9db5398b2d04faf6cca1c9e52736f99
theedigerati <39467790+theedigerati@users.noreply.github.com>
2024-02-28 05:52:28 +0100

    add impl for TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let lo = 0;
+    let hi = breaks.length;
 
+    while (lo < hi) {
+        const jump = Math.floor(lo + Math.sqrt(hi - lo));
+
+        if (breaks[jump]) {
+            for (let i = lo; i < jump; ++i) {
+                if (breaks[i]) {
+                    return i;
+                }
+            }
+            return jump;
+        }
+        lo = jump;
+    }
+    return -1;
 }
