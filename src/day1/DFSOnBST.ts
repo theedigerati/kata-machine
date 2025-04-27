2497bcf2be81d2b2416eb022982dbe6164088829
theedigerati <39467790+theedigerati@users.noreply.github.com>
2025-04-27 23:52:49 +0100

    add solution for DFS on a BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,13 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
+export default function dfs(head: BinaryNode<number> | null, needle: number): boolean {
+    if (head === null){
+        return false
+    }
+    if (needle === head.value){
+        return true
+    }
+    if (needle <= head.value){
+        return dfs(head.left, needle)
+    }else{
+        return dfs(head.right, needle)
+    }
 }
