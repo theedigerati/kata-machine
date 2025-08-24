caff51c4ca3b2f0ef5d65f38f04ae2a8b8e75999
theedigerati <39467790+theedigerati@users.noreply.github.com>
2025-08-25 00:17:32 +0100

    add recursive delete method for MinHeap solution

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -14,6 +14,46 @@ export default class MinHeap {
         this.heapifyUp(this.length - 1);
     }
 
+    delete(): number {
+        if (this.length === 0) {
+            return -1;
+        }
+
+        const out = this.data[0];
+        if (this.length === 1) {
+            this.data = [];
+            return out;
+        }
+
+        this.data[0] = this.data[this.length - 1];
+        this.data.pop();
+        this.heapifyDown(0);
+        return out;
+    }
+
+    private heapifyDown(idx: number) {
+        const lIdx = this.leftChild(idx);
+        const rIdx = this.rightChild(idx);
+
+        if (idx >= this.length || lIdx >= this.length) {
+            return;
+        }
+
+        const lV = this.data[lIdx];
+        const rV = this.data[rIdx];
+        const v = this.data[idx];
+
+        if (lV > rV && v > rV) {
+            this.data[rIdx] = v;
+            this.data[idx] = rV;
+            this.heapifyDown(rIdx);
+        } else if (rV > lV && v > lV) {
+            this.data[lIdx] = v;
+            this.data[idx] = lV;
+            this.heapifyDown(lIdx);
+        }
+    }
+
     private heapifyUp(idx: number) {
         if (idx === 0) {
             return;
@@ -32,6 +72,14 @@ export default class MinHeap {
 
     private parent(idx: number): number {
         return Math.floor((idx - 1) / 2);
+    }
+
+    private leftChild(idx: number): number {
+        return 2 * idx + 1;
+    }
+
+    private rightChild(idx: number): number {
+        return 2 * idx + 2;
     }
 
     _insert(value: number): void {
@@ -56,7 +104,7 @@ export default class MinHeap {
         }
     }
 
-    delete(): number {
+    _delete(): number {
         const out = this.data[0];
         const tail = this.data.pop();
 
